73ce61f47893196d8de7f312f46ab95f8b93b6c4
userlg <guipeluis90@gmail.com>
2021-07-09 13:25:34 -0400

    This is a rest api with flask

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from app import app
+
+
+@app.route('/')
+def home():
+    return '<h1> Welcome to the home </h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h1> This is about route </h1>'
